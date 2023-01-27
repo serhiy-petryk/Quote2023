5105af8c15e8aaec64fc0ef656de277319306042
serhiy-petryk <sergei_petrik@yahoo.com>
2023-01-27 04:47:42 +0200

    x

--- Quote2023/spMain/Comp/frmUIStockGraph.cs
+++ Quote2023/spMain/Comp/frmUIStockGraph.cs
@@ -1,19 +1,25 @@
 using System;
 using System.Windows.Forms;
 
-namespace spMain.Comp {
-  public partial class frmUIStockGraph : Form {
-    public frmUIStockGraph() {
-      InitializeComponent();
-    }
-
-    private void frmUIStockGraph_Load(object sender, EventArgs e) {
-      if (!csIni.isDesignMode) {
-        object x = cs.PGfrmObjectEditor._GetObject(new spMain.QData.UI.UIGraph(), QData.UI.UIGraph._serializationFileName, true);
-        if (x != null) {
-          this.uI_StockGraph1._SetUIGraph((QData.UI.UIGraph)x);
+namespace spMain.Comp
+{
+    public partial class frmUIStockGraph : Form
+    {
+        public frmUIStockGraph()
+        {
+            InitializeComponent();
         }
-      }
+
+        private void frmUIStockGraph_Load(object sender, EventArgs e)
+        {
+            if (!csIni.isDesignMode)
+            {
+                var x = cs.PGfrmObjectEditor._GetObject(new spMain.QData.UI.UIGraph(), QData.UI.UIGraph._serializationFileName, true);
+                if (x == null)
+                    BeginInvoke(new MethodInvoker(Close));
+                else
+                    uI_StockGraph1._SetUIGraph((QData.UI.UIGraph)x);
+            }
+        }
     }
-  }
 }
