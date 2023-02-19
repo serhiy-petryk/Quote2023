51f50d766928e94a3ab551ba22b5fa6d567380b6
serhiy-petryk <sergei_petrik@yahoo.com>
2023-02-19 16:01:31 +0200

    Update Main.cs

--- Quote2023/ProxyChecker/Main.cs
+++ Quote2023/ProxyChecker/Main.cs
@@ -10,6 +10,10 @@ using System.Windows.Forms;
 
 namespace ProxyChecker
 {
+    // https://www.proxy-list.download/api/v1/get?type=https
+    // https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all
+    // sites: https://www.proxy-list.download/HTTP, https://free-proxy-list.net/
+
     public partial class Main : Form
     {
         private bool isClosing;
@@ -21,24 +25,26 @@ namespace ProxyChecker
 
         private string[] proxies;
 
-        private void GetProxies(string proxyType)
+        private void StartCheckProxies()
         {
-            if (proxyType == "api")
+            /*if (proxyType == "api")
                 GetProxiesFromApi();
             else
-                GetProxiesFromFile(@"E:\Temp\ProjectTests\ProxyList1.txt");
+                GetProxiesFromFile(@"E:\Temp\ProjectTests\ProxyList1.txt");*/
 
             proxies = proxies.Distinct().ToArray();
             label_TotalProxy.Text = proxies.Length.ToString();
+            label_GoodProxy.Text = "0";
+            label_BadProxy.Text = "0";
 
             var thread = new Thread(SubTask);
             thread.Start();
             thread.IsBackground = true;
         }
 
-        private void GetProxiesFromApi()
+        private void GetProxiesFromApi(string url)
         {
-            var url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all";
+            // var url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all";
             // string url = @"https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt";
 
             using (var request = new WebClient())
@@ -46,11 +52,14 @@ namespace ProxyChecker
                 var response = request.DownloadString(url).ToString();
                 proxies = response.Split(new[] {Environment.NewLine, "\n"}, StringSplitOptions.None);
             }
+
+            StartCheckProxies();
         }
 
         private void GetProxiesFromFile(string filename)
         {
             proxies = File.ReadAllLines(filename).Where(a => !string.IsNullOrEmpty(a) && !a.StartsWith("#")).Select(a => a.Split('\t')[0].Trim()).ToArray();
+            StartCheckProxies();
         }
 
         [DebuggerStepThrough]
@@ -62,6 +71,9 @@ namespace ProxyChecker
             {
                 try
                 {
+                    request.Headers[HttpRequestHeader.UserAgent] = @"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
+                    // request.Headers[HttpRequestHeader.UserAgent] = @"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
+                    // request.Headers[HttpRequestHeader.UserAgent] = @"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)";
                     // request.Proxy = HttpProxyClient.Parse(proxy);
                     request.Proxy = new WebProxy(proxy);
 
@@ -107,8 +119,23 @@ namespace ProxyChecker
             MessageBox.Show("Finished");
         }
 
-        private void button_StartApi_Click(object sender, EventArgs e) => GetProxies("api");
-        private void btnStartFile_Click(object sender, EventArgs e) => GetProxies("file");
+        private void button_StartApi_Click(object sender, EventArgs e) => GetProxiesFromApi( "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all");
+
+        private void btnStartFile_Click(object sender, EventArgs e)
+        {
+            var folder = @"E:\Temp\ProjectTests";
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = folder;
+                ofd.RestoreDirectory = true;
+                ofd.Multiselect = false;
+                ofd.Filter = "Text|*.txt|All|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    GetProxiesFromFile(ofd.FileName);
+                }
+            }
+        }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e) => isClosing = true;
     }
